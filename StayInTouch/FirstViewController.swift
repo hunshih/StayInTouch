1b5d1c73a304c29ddfd9be70217438e8ed3f57a3
Hung-Yuan Shih <henry@hung-yuans-mini.home>
2017-02-11 19:55:52 -0500

    Initial Commit

---
+++ StayInTouch/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  StayInTouch
+//
+//  Created by Hung-Yuan Shih on 2/11/17.
+//  Copyright © 2017 Hung-Yuan Shih. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
