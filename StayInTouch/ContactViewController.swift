06bbc2500e1bba5286c01e72f91d179494081c0d
Hung-Yuan Shih <henry@macbook-pro.home>
2017-03-25 17:17:19 -0400

    Added controllers for adding contacts

---
+++ StayInTouch/ContactViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ContactViewController.swift
+//  StayInTouch
+//
+//  Created by Hung-Yuan Shih on 3/25/17.
+//  Copyright © 2017 Hung-Yuan Shih. All rights reserved.
+//
+
+import UIKit
+
+class ContactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cancelToContactViewController(segue:UIStoryboardSegue) {
+    }
+    
+    @IBAction func ContactAdded(segue:UIStoryboardSegue) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
