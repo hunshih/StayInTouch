32886bbff9d3e3cbc5f611445ae9c2a4487fac97
Hung-Yuan Shih <henry@macbook-pro.home>
2017-06-19 23:54:24 -0400

    Changed the signing/signup flow Need to complete Account Create flow

---
+++ StayInTouch/WelcomeViewController.swift
@@ -0,0 +1,39 @@
+//
+//  WelcomeViewController.swift
+//  StayInTouch
+//
+//  Created by Hung-Yuan Shih on 6/19/17.
+//  Copyright © 2017 Hung-Yuan Shih. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backToWelcome(segue:UIStoryboardSegue) {
+        let source = segue.source as! SignInViewController;
+        print("Return to welcome!");
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
