1d935bd736fd1dd644916a4bdb2bf77ef8a04362
Hung-Yuan Shih <henry@macbook-pro.home>
2017-08-25 23:12:32 -0400

    disabled cell slide delete

--- StayInTouch/ConversationTableViewController.swift
+++ StayInTouch/ConversationTableViewController.swift
@@ -78,7 +78,8 @@ class ConversationTableViewController: UITableViewController {
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
-        return true
+        //return true
+        return false;
     }
     
     
