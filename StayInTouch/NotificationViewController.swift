df0dad755c04b271d100d9de53a9133a5c7255f8
Hung-Yuan Shih <henry@macbook-pro.home>
2017-08-28 23:53:05 -0400

    Fixed no email account bug

--- StayInTouch/NotificationViewController.swift
+++ StayInTouch/NotificationViewController.swift
@@ -50,8 +50,15 @@ class NotificationViewController: UIViewController, MFMailComposeViewControllerD
         mc.setSubject(emailTitle!);
         mc.setMessageBody(messageBody, isHTML: false);
         mc.setToRecipients(toRecipents);
-        
-        self.present(mc, animated: true, completion: nil)
+        if MFMailComposeViewController.canSendMail()
+        {
+            self.present(mc, animated: true, completion: nil)
+        }
+        else
+        {
+            let sendMailErrorAlert = UIAlertView(title: "Could Not Send Email", message: "Your device could not send e-mail.  Please check e-mail configuration and try again.", delegate: self, cancelButtonTitle: "OK")
+            sendMailErrorAlert.show()
+        }
     }
     @objc(mailComposeController:didFinishWithResult:error:)
     func mailComposeController(controller:MFMailComposeViewController, didFinishWithResult result:MFMailComposeResult, error:NSError) {
